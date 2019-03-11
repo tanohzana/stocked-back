96be07cd79157efa808cd423a86e2fe4a6324ece
tanohzana <tanohzana@gmail.com>
2019-03-11 12:58:32 +0100

    feat: server uses websockets and fetches default stocks

---
+++ src/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/robfig/cron"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
+var serverPort = ":8080"
+var defaultStocks = "aapl,fb,tsla"
+var iexURL = "https://api.iextrading.com"
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleStockChangesWithCron()
+
+	log.Println("Magic is happening on port ", serverPort)
+	err := http.ListenAndServe(serverPort, nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Headers nil here
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ws.Close()
+
+	clients[ws] = true
+}
+
+func handleStockChangesWithCron() {
+	c := cron.New()
+	c.AddFunc("@every 10s", cronTask)
+
+	c.Start()
+}
+
+func cronTask() {
+	fmt.Println("Fetching stocks")
+	resp, err1 := http.Get(iexURL + "/1.0/stock/market/batch?symbols=" + defaultStocks + "&types=quote,chart&range=1m&last=5")
+
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	stocks, err2 := ioutil.ReadAll(resp.Body)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	resp.Body.Close()
+
+	fmt.Println(string(stocks))
+}
